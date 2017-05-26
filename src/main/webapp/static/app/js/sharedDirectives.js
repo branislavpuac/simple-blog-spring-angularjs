7532674275dffab3a09c2f567c39d5b3bede59e3
Branislav Puac <branislavpuac@gmail.com>
2017-05-26 14:08:48 +0000

    added login, register form validation and compareTo directive

--- src/main/webapp/static/app/js/sharedDirectives.js
+++ src/main/webapp/static/app/js/sharedDirectives.js
@@ -15,3 +15,21 @@ sharedDirectives.directive('fileModel', ['$parse', function ($parse) {
         }
     };
 }]);
+
+blog.directive('compareTo', [
+	function() {
+		return {
+			require: "ngModel",
+			scope: { otherModelValue: "=compareTo" },
+      link: function(scope, element, attributes, ngModel) {
+    	  		ngModel.$validators.compareTo = function(modelValue) {
+    	  			return modelValue == scope.otherModelValue;
+    	  		};
+    	  		scope.$watch("otherModelValue", function() {
+    	  			ngModel.$validate();
+    	  		});
+      }
+    };
+  }
+]);
+
